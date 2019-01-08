6e8befa8b1b98fec5a5927f5799d161dd81e6fed
Sukruti Admuthe <sukruti.admuthe@gmail.com>
2019-01-08 12:05:04 +0530

    Delete Demo.py

--- Demo.py
+++
@@ -1,6 +0,0 @@
-num1=input('Hey Please enter 1st number')
-num2=input('Hey Please enter 2nd number')
-tot=num1+num2
-print('Add of {num1} and {num2} is {tot}'.format(num1=num1,  num2=num2,tot = tot))
-str=raw_input("PLease enterr the epression")
-print(eval(str))
